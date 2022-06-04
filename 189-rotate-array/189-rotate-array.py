a2bec3e9f68d7bfccd66d5d34dacbcd21b28745b
Vishwa Shrirame <44528257+vishwa3011@users.noreply.github.com>
2022-06-04 17:37:29 -0400

    Time: 227 ms (87.23%), Space: 25.5 MB (6.73%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.py
@@ -0,0 +1,36 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        
+        nums = self.swap(0, len(nums)-1, nums)
+        nums = self.swap(0, k-1, nums)
+        nums = self.swap(k, len(nums)-1, nums)
+        
+    def swap(self, i, j, nums):
+        while i < j:
+            temp = nums[i]
+            nums[i] = nums[j]
+            nums[j] = temp
+            i += 1
+            j -= 1  
+        return nums
+        
+            
+            
+#         rotated_arr = []
+#         n = len(nums)
+#         temp = k
+#         while temp > 0:
+#             rotated_arr.append(nums[n - temp])
+#             temp -= 1
+        
+#         i = 0
+#         while i <= k:
+#             rotated_arr.append(nums[i])
+#             i += 1
+        
+#         print(rotated_arr)
+            
