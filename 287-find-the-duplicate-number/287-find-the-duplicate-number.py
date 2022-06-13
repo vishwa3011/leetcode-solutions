835495efb566403c0ecd962562ea5e4e3595414b
Vishwa Shrirame <44528257+vishwa3011@users.noreply.github.com>
2022-06-12 20:37:00 -0400

    Time: 742 ms (68.81%), Space: 28 MB (55.50%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -1,17 +1,28 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         
-        dups = {}
-        
+        '''
+        1) flip the index sign for a specific number in the list
+        '''
         for i in nums:
-            if i not in dups:
-                dups[i] = 1
-            else:
-                dups[i] += 1
-        print(dups)
+            current = abs(i)
+            if nums[current] < 0:
+                return current
+            nums[current] = -nums[current]
         
-        for key,val in dups.items():
-            if val > 1:
-                return key
         
         return -1
+                
+#         dups = {}
+        
+#         for i in nums:
+#             if i not in dups:
+#                 dups[i] = 1
+#             else:
+#                 dups[i] += 1
+        
+#         for key,val in dups.items():
+#             if val > 1:
+#                 return key
+        
+#         return -1
