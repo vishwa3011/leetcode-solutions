c3b3335fab6f6c4b54d416e8bf0b19f8f34bff93
Vishwa Shrirame <44528257+vishwa3011@users.noreply.github.com>
2022-11-03 13:03:22 -0400

    Time: 466 ms (15.59%), Space: 15.5 MB (34.63%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,15 +1,35 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        num_counts = {}
+        # Time - O(n) Space - O(n)
+#         num_counts = {}
         
-        for i in range(len(nums)):
-            if nums[i] not in num_counts:
-                num_counts[nums[i]] = 1
-            else:
-                num_counts[nums[i]] += 1
+#         for i in range(len(nums)):
+#             if nums[i] not in num_counts:
+#                 num_counts[nums[i]] = 1
+#             else:
+#                 num_counts[nums[i]] += 1
             
-        for key, val in num_counts.items():
-            if val > len(nums)/2:
-                return key
+#         for key, val in num_counts.items():
+#             if val > len(nums)/2:
+#                 return key
         
-        return -1
+#         return -1
+    
+        ## Boyer Moore Voting Algorithm
+        # Time - O(n) Space - O(1)
+        
+        count = 0
+        candidate = None
+        
+        for num in nums:
+            
+            if count == 0:
+                candidate = num
+                
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+        
+        return candidate
+        
