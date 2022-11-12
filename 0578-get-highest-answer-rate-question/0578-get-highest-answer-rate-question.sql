7d0ed2ba6999034b3ce0eef3fa516cbfba1c4cf2
Vishwa Shrirame <44528257+vishwa3011@users.noreply.github.com>
2022-11-12 13:24:21 -0500

    Time: 404 ms (39.72%), Space: 0B (100.00%) - LeetHub

--- 0578-get-highest-answer-rate-question/0578-get-highest-answer-rate-question.sql
+++ 0578-get-highest-answer-rate-question/0578-get-highest-answer-rate-question.sql
@@ -1,25 +1,12 @@
 # Write your MySQL query statement below
 
-with answer_cte
-as
-(
-select question_id, sum(case when action = 'answer' then 1 else 0 end) as answer_cnt
-    from surveylog
-    group by question_id
-),
-show_cte
-as
-(
-select question_id, sum(case when action = 'show' then 1 else 0 end) as show_cnt
-    from surveylog
-    group by question_id
-),
+with 
 answer_rate_cte
 as
 (
-select a.question_id, answer_cnt/b.show_cnt as answer_rate
-    from answer_cte a join show_cte b
-    on a.question_id = b.question_id
+select question_id, sum(case when action = 'answer' then 1 else 0 end)/sum(case when action = 'show' then 1 else 0 end) as answer_rate
+    from surveylog
+    group by question_id
 )
 select question_id as survey_log
 from answer_rate_cte
