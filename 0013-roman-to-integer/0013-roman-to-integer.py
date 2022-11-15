e605284f17b75466cbd7836ff7cbc181391ba9d7
Vishwa Shrirame <44528257+vishwa3011@users.noreply.github.com>
2022-11-14 23:46:08 -0500

    Time: 123 ms (12.09%), Space: 13.9 MB (30.69%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,22 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        
+        hashmap = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
+        integer = 0
+        
+        i = 0
+        
+        while i < len(s):
+            
+            if i < len(s) - 1:
+                if (s[i] == 'I' and s[i+1] == 'V') or (s[i] == 'I' and s[i+1] == 'X') or (s[i] == 'X' and s[i+1] == 'L') or (s[i] == 'X' and s[i+1] == 'C') or (s[i] == 'C' and s[i+1] == 'D') or (s[i] == 'C' and s[i+1] == 'M'):
+                    integer += hashmap[s[i+1]] - hashmap[s[i]]
+                    i += 1
+                else:
+                    integer += hashmap[s[i]]
+            else:
+                integer += hashmap[s[i]]
+            
+            i += 1
+            
+        return integer
