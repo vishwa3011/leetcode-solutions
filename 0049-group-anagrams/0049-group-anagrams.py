dd69129d451548751108f4ddee331308cdecaf3c
Vishwa Shrirame <44528257+vishwa3011@users.noreply.github.com>
2022-11-16 13:37:39 -0500

    Time: 265 ms (26.31%), Space: 19.1 MB (29.97%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,33 @@
+from collections import Counter, defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        group_anagrams = defaultdict(list)
+        ans = []
+        # count_chars = [0]*26
+        
+        for word in strs:
+            count_chars = [0]*26
+            for char in word:
+                count_chars[ord(char) - ord('a')] += 1
+            group_anagrams[tuple(count_chars)].append(word)
+        
+        return list(group_anagrams.values())
+    
+        ## Brute Force (gives TLE for big inputs)
+#         ans = []
+#         idx = []
+        
+#         for i in range(len(strs)):
+#             anagrams = []
+#             if i not in idx:
+#                 anagrams.append(strs[i])
+#                 for j in range(len(strs)):
+#                     if j not in idx:
+#                         if Counter(strs[i]) == Counter(strs[j]) and i != j:
+#                             anagrams.append(strs[j])
+#                             idx.append(j)
+                    
+#                 ans.append(anagrams)
+        
+#         return ans
