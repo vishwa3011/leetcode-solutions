3dfe1ce664d86ae007b9cfa5e5db3b0b64fa2c15
Vishwa Shrirame <44528257+vishwa3011@users.noreply.github.com>
2022-11-17 23:01:11 -0500

    Time: 2329 ms (58.99%), Space: 25 MB (39.10%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -3,21 +3,19 @@ class Solution:
         
         ## maximum profit is buying at a very less price and selling on a very high price
         
-        j = 1
         buy_price = float('inf')
         max_profit = 0
         
-        for i in range(len(prices)-1):
+        for i in range(len(prices)):
             
             if prices[i] < buy_price:
                 buy_price = prices[i]
                 
-            sell_price = prices[j]
+            sell_price = prices[i]
             profit = sell_price - buy_price
             
             if profit > max_profit:
                 max_profit = profit
             
-            j += 1
             
         return max_profit
