cf29a0345fd818f72eff2d2f23c5a662b30abad1
Vishwa Shrirame <44528257+vishwa3011@users.noreply.github.com>
2022-11-19 21:17:07 -0500

    Time: 476 ms (44.89%), Space: 0B (100.00%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,9 +1,6 @@
 # Write your MySQL query statement below
 
-with cte as(
-select id, email, row_number() over(partition by email order by id) as rn
-from person
-)
-
-select distinct email
-from cte where rn != 1
+select email
+from person 
+group by email
+having count(email) > 1
