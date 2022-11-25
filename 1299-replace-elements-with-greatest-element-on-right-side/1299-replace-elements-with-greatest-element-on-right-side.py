4ff10b40615ebccf2197f4fa055a9aba3c854e4b
Vishwa Shrirame <44528257+vishwa3011@users.noreply.github.com>
2022-11-24 23:19:45 -0500

    Time: 1168 ms (24.63%), Space: 15.7 MB (6.72%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
@@ -1,27 +1,27 @@
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
         
-        if not arr:
-            return []
-        else:
-            greatest_elements = [-1]
+        # if not arr:
+        #     return []
+        # else:
+        #     greatest_elements = [-1]
             
-        max_val = 0
+        max_val = -1
         i = len(arr) - 1
         
-        while i > 0:
-            if arr[i] > max_val:
-                max_val = arr[i]
-            
+        while i >= 0:
+            arr[i], max_val = max_val, max(max_val, arr[i])
             i -= 1   
-            greatest_elements.append(max_val)
+            #greatest_elements.append(max_val)
+            
+        return arr
         
-        j = 0
-        k = len(greatest_elements) - 1
+#         j = 0
+#         k = len(greatest_elements) - 1
         
-        while j <= k:
-            greatest_elements[j], greatest_elements[k] = greatest_elements[k], greatest_elements[j]
-            j += 1
-            k -= 1
+#         while j <= k:
+#             greatest_elements[j], greatest_elements[k] = greatest_elements[k], greatest_elements[j]
+#             j += 1
+#             k -= 1
         
-        return greatest_elements    
+#         return greatest_elements    
