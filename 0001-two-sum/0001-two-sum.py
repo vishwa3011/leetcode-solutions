1d275d7f53410d1ae6a00d366cca4d9cb3d16e26
Vishwa Shrirame <44528257+vishwa3011@users.noreply.github.com>
2023-03-09 18:00:58 -0500

    Time: 64 ms (71.10%), Space: 15.1 MB (37.99%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,9 +2,10 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         h = {}
         ans = [0,0]
+        
         for i in range(len(nums)):
             h[nums[i]] = i
-        print(h)
+            
         for i in range(len(nums)):
             complement = target - nums[i]
             
